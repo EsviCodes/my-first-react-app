ca574d261aae4a947af78e2f4b97d64e683dc940
EsviCodes <esther.js.vis@gmail.com>
2019-09-30 13:42:51 +0200

    first proper component

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
   return (
     <div className="App">
       <main>
-        <Title />
+        <Title content="Some Simple Title" />
       </main>
 
       <header className="App-header">
