e87044942e2ac7c3a5f0936fa25830c3e29fed4e
EsviCodes <esther.js.vis@gmail.com>
2019-09-30 11:11:49 +0200

    First commit - Hello world

--- src/App.js
+++ src/App.js
@@ -1,23 +1,18 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        <h1> Hello World </h1>
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          {" "}
+          It's fun that it works like this, but I don't like the fact that my
+          theme in JavasScript doesn't show me the difference of an element of
+          my normal text
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
       </header>
     </div>
   );
